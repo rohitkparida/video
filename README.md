73181f53f245aa195eedf0b6e420916e90119d1d
Rohit Parida <rohitparida27@gmail.com>
2017-11-23 20:39:02 +0530

    Initial

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# video
+PushUp
+
+A Push up counting app.
+
+========
+
+#F.A.Q-
+1. How to does it count Push ups?
+- Simply place your smartphone in front of your face, facing upwards while being in a Push up stance then with each Push up tap on screen with your nose tip.
+
+========
+
+# Features
+
+- Offline-capable - ServiceWorker. (<https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API>)
+- "Add To Home Screen" capable.
---
+++ js/app.js
@@ -0,0 +1,40 @@
+(function localFileVideoPlayer() {
+	'use strict'
+  var URL = window.URL || window.webkitURL
+  var displayMessage = function (message, isError) {
+    var element = document.querySelector('#message')
+    element.innerHTML = message
+    element.className = isError ? 'error' : 'info'
+  }
+  var playSelectedFile = function (event) {
+    var file = this.files[0]
+    var type = file.type
+    var videoNode = document.querySelector('video')
+    var canPlay = videoNode.canPlayType(type)
+    if (canPlay === '') canPlay = 'no'
+    var message = 'Can play type "' + type + '": ' + canPlay
+    var isError = canPlay === 'no'
+    displayMessage(message, isError)
+
+    if (isError) {
+      return
+    }
+
+    var fileURL = URL.createObjectURL(file)
+    videoNode.src = fileURL
+  }
+  var inputNode = document.querySelector('input')
+  inputNode.addEventListener('change', playSelectedFile, false)
+})()
+
+
+// Registering ServiceWorker
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('sw.js').then(function(registration) {
+    // Registration was successful
+    console.log('ServiceWorker registration successful with scope: ',    registration.scope);
+  }).catch(function(err) {
+    // registration failed :(
+    console.log('ServiceWorker registration failed: ', err);
+  });
+}
