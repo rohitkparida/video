96e7596cd9bbac2fa8cb38f0cddcf7e46278864c
Rohit Parida <rohitparida27@gmail.com>
2017-12-08 17:19:56 +0530

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -28,6 +28,86 @@
 })()
 
 
+// Pause control
+// $("video").click(function() {
+//   //console.log(this);
+//   if (this.paused) {
+//     this.play();
+//   } else {
+//     this.pause();
+//   }
+// });
+
+// ____________________________SWIPE
+$(document).ready(function() {
+
+var element = $('#element');
+var debug = $('#debug');
+
+element.touch = {}
+
+ element.click(function() {
+  console.log(this);
+  if (this.paused) {
+    this.play();
+  } else {
+    this.pause();
+  }
+});
+element.get(0).ontouchstart = function(e) {
+  // Store start position
+  element.touch.x = e.touches[0].clientX;
+};
+
+element.get(0).ontouchmove = function(e) {
+  // only deal with one finger
+  if (e.touches.length == 1 && !element.is(":animated")) {
+
+    // Get delta
+    var deltaX = element.touch.x - e.touches[0].clientX;
+
+    // Move foward
+    if (deltaX > 20) {
+        this.currentTime -= .5;
+    }
+    // Move backward
+    else if (deltaX < -20)  {
+		this.currentTime += .5;
+    }
+  }
+};
+
+});
+
+
+//DIM Control_______________________________
+$('#contrast').on('input', function() {
+    $('#contrastFilter').css('opacity', $(this).val());
+});
+
+// ___________DEFAULT_VOLUME
+$("video").prop("volume", 0.5);
+
+
+//URL______________________________
+function changeURL() {
+    var newURL = document.getElementById("newURL").value;
+	var x;
+	x = document.getElementById("newURL").value;
+    if (x == "")
+    document.getElementById("element").src=newURL;
+}
+
+function empty() {
+    var x;
+    x = document.getElementById("newURL").value;
+    if (x == "") {
+        alert("Enter a Valid Roll Number");
+        return false;
+    };
+}
+
+
 // Registering ServiceWorker
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('sw.js').then(function(registration) {
