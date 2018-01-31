a2f7fcee10f0b4b4b0fc17612b9220933e42cd5a
Rohit Parida <rohitparida27@gmail.com>
2018-01-31 15:20:20 +0530

    ui

--- js/app.js
+++ js/app.js
@@ -1,30 +1,30 @@
 (function localFileVideoPlayer() {
-	'use strict'
-  var URL = window.URL || window.webkitURL
-  var displayMessage = function (message, isError) {
-    var element = document.querySelector('#message')
-    element.innerHTML = message
-    element.className = isError ? 'error' : 'info'
-  }
-  var playSelectedFile = function (event) {
-    var file = this.files[0]
-    var type = file.type
-    var videoNode = document.querySelector('video')
-    var canPlay = videoNode.canPlayType(type)
-    if (canPlay === '') canPlay = 'no'
-    var message = 'Can play type "' + type + '": ' + canPlay
-    var isError = canPlay === 'no'
-    displayMessage(message, isError)
-
-    if (isError) {
-      return
+    'use strict'
+    var URL = window.URL || window.webkitURL
+    var displayMessage = function(message, isError) {
+        var element = document.querySelector('#message')
+        element.innerHTML = message
+        element.className = isError ? 'error' : 'info'
     }
+    var playSelectedFile = function(event) {
+        var file = this.files[0]
+        var type = file.type
+        var videoNode = document.querySelector('video')
+        var canPlay = videoNode.canPlayType(type)
+        if (canPlay === '') canPlay = 'no'
+        var message = 'Can play type "' + type + '": ' + canPlay
+        var isError = canPlay === 'no'
+        displayMessage(message, isError)
 
-    var fileURL = URL.createObjectURL(file)
-    videoNode.src = fileURL
-  }
-  var inputNode = document.querySelector('input')
-  inputNode.addEventListener('change', playSelectedFile, false)
+        if (isError) {
+            return
+        }
+
+        var fileURL = URL.createObjectURL(file)
+        videoNode.src = fileURL
+    }
+    var inputNode = document.querySelector('input')
+    inputNode.addEventListener('change', playSelectedFile, false)
 })()
 
 
@@ -41,48 +41,48 @@
 // ____________________________SWIPE
 $(document).ready(function() {
 
-var element = $('#element');
-var debug = $('#debug');
+    var element = $('#element');
+    var debug = $('#debug');
 
-element.touch = {}
+    element.touch = {}
 
- element.click(function() {
-  console.log(this);
-  if (this.paused) {
-    this.play();
-  } else {
-    this.pause();
-  }
-});
-element.get(0).ontouchstart = function(e) {
-  // Store start position
-  element.touch.x = e.touches[0].clientX;
-};
+    element.click(function() {
+        console.log(this);
+        if (this.paused) {
+            this.play();
+        } else {
+            this.pause();
+        }
+    });
+    element.get(0).ontouchstart = function(e) {
+        // Store start position
+        element.touch.x = e.touches[0].clientX;
+    };
 
-element.get(0).ontouchmove = function(e) {
-  // only deal with one finger
-  if (e.touches.length == 1 && !element.is(":animated")) {
+    element.get(0).ontouchmove = function(e) {
+        // only deal with one finger
+        if (e.touches.length == 1 && !element.is(":animated")) {
 
-    // Get delta
-    var deltaX = element.touch.x - e.touches[0].clientX;
+            // Get delta
+            var deltaX = element.touch.x - e.touches[0].clientX;
 
-    // Move foward
-    if (deltaX > 20) {
-        this.currentTime -= .5;
-    }
-    // Move backward
-    else if (deltaX < -20)  {
-		this.currentTime += .5;
-    }
-  }
-};
+            // Move foward
+            if (deltaX > 20) {
+                this.currentTime -= .5;
+            }
+            // Move backward
+            else if (deltaX < -20) {
+                this.currentTime += .5;
+            }
+        }
+    };
 
 });
 
 
 //DIM Control_______________________________
 $('#contrast').on('input', function() {
-    $('#contrastFilter').css('opacity', $(this).val());
+    $('#element').css('opacity', $(this).val());
 });
 
 // ___________DEFAULT_VOLUME
@@ -92,17 +92,17 @@ $("video").prop("volume", 0.5);
 //URL______________________________
 function changeURL() {
     var newURL = document.getElementById("newURL").value;
-    document.getElementById("element").src=newURL;
+    document.getElementById("element").src = newURL;
 }
 
 
 // Registering ServiceWorker
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('sw.js').then(function(registration) {
-    // Registration was successful
-    console.log('ServiceWorker registration successful with scope: ',    registration.scope);
-  }).catch(function(err) {
-    // registration failed :(
-    console.log('ServiceWorker registration failed: ', err);
-  });
+    navigator.serviceWorker.register('sw.js').then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }).catch(function(err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+    });
 }
